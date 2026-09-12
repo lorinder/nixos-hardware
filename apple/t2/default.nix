1437e45fd117f97390ba372241e58c0608b21326
Lorenz Minder <lminder@gmx.net>
2026-09-12 13:35:44 -0500

    Enable t2bce.

--- apple/t2/default.nix
+++ apple/t2/default.nix
@@ -85,7 +85,7 @@ in
           if t2Cfg.kernelChannel == "stable" then ./pkgs/linux-t2 else ./pkgs/linux-t2/latest.nix
         ) { }
       );
-      boot.initrd.kernelModules = [ "apple-bce" ];
+      boot.initrd.kernelModules = [ "t2bce_core" "t2bce_dma" "t2bce_vhci" ];
 
       services.udev.packages = [ audioFilesUdevRules ];
 
--- apple/t2/pkgs/linux-t2/generic.nix
+++ apple/t2/pkgs/linux-t2/generic.nix
@@ -44,6 +44,10 @@ kernel.override (
       HID_SENSOR_ALS = module;
       SND_PCM = module;
       STAGING = yes;
+      T2BCE_AUDIO = module;
+      T2BCE_CORE = module;
+      T2BCE_DMA = module;
+      T2BCE_VHCI = module;
     };
 
     kernelPatches = t2-patches ++ (args.kernelPatches or [ ]);
