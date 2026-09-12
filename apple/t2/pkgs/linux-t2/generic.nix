487c56afcec78185fca329cb8c9b64ab7a6ec761
Lorenz Minder <lminder@gmx.net>
2026-09-12 09:46:08 -0500

    Another fix, get rid of APPLE_BCE.

--- apple/t2/pkgs/linux-t2/generic.nix
+++ apple/t2/pkgs/linux-t2/generic.nix
@@ -29,7 +29,6 @@ kernel.override (
     pname = "linux-t2";
 
     structuredExtraConfig = with lib.kernel; {
-      APPLE_BCE = module;
       APPLE_GMUX = module;
       APFS_FS = module;
       BRCMFMAC = module;
